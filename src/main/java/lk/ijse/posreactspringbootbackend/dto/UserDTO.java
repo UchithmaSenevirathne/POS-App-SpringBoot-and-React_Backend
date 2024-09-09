80b29f18bafb854921d0f6abdaed730ffcba285d
UchithmaSenevirathne <uchithma@gmail.com>
2024-09-09 19:50:20 +0530

    create user dto

--- src/main/java/lk/ijse/posreactspringbootbackend/dto/UserDTO.java
+++ src/main/java/lk/ijse/posreactspringbootbackend/dto/UserDTO.java
@@ -1,4 +1,18 @@
 package lk.ijse.posreactspringbootbackend.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class UserDTO {
+    private String userId;
+    private String name;
+    private String address;
+    private String contact;
+    private String email;
+    private String password;
+    private String profilePicture;
 }

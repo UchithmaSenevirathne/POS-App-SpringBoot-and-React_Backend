48c06bdc87b16bb4a478c610c963c556bc2425ab
UchithmaSenevirathne <uchithma@gmail.com>
2024-09-09 19:49:06 +0530

    create user entity

--- src/main/java/lk/ijse/posreactspringbootbackend/entity/UserEntity.java
+++ src/main/java/lk/ijse/posreactspringbootbackend/entity/UserEntity.java
@@ -1,4 +1,27 @@
 package lk.ijse.posreactspringbootbackend.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Table(name = "user")
 public class UserEntity {
+    @Id
+    private String userId;
+    private String name;
+    private String address;
+    private String contact;
+    @Column(unique = true)
+    private String email;
+    private String password;
+    @Column(columnDefinition = "LONGTEXT")
+    private String profilePicture;
 }

4b56186e8eaf14acffc1415613cb53c3f2d03449
UchithmaSenevirathne <uchithma@gmail.com>
2024-09-11 19:40:50 +0530

    add some content

--- src/main/java/lk/ijse/posreactspringbootbackend/controller/ItemController.java
+++ src/main/java/lk/ijse/posreactspringbootbackend/controller/ItemController.java
@@ -1,4 +1,18 @@
 package lk.ijse.posreactspringbootbackend.controller;
 
+import lk.ijse.posreactspringbootbackend.service.ItemService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/backend/item")
+@CrossOrigin
+@RequiredArgsConstructor
 public class ItemController {
+
+    @Autowired
+    private final ItemService itemService;
 }
--- src/main/java/lk/ijse/posreactspringbootbackend/service/impl/ItemServiceImpl.java
+++ src/main/java/lk/ijse/posreactspringbootbackend/service/impl/ItemServiceImpl.java
@@ -1,6 +1,19 @@
 package lk.ijse.posreactspringbootbackend.service.impl;
 
+import lk.ijse.posreactspringbootbackend.dao.ItemDAO;
 import lk.ijse.posreactspringbootbackend.service.ItemService;
+import lk.ijse.posreactspringbootbackend.util.Mapping;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+@Service
+@Transactional
 public class ItemServiceImpl implements ItemService {
+
+    @Autowired
+    private ItemDAO itemDAO;
+
+    @Autowired
+    private Mapping mapping;
 }

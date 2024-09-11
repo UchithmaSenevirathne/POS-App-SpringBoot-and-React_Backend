d9b29be9618e027d44d1ddaa3576633f28939dc0
UchithmaSenevirathne <uchithma@gmail.com>
2024-09-11 19:41:50 +0530

    implement item dao

--- src/main/java/lk/ijse/posreactspringbootbackend/dao/ItemDAO.java
+++ src/main/java/lk/ijse/posreactspringbootbackend/dao/ItemDAO.java
@@ -1,4 +1,9 @@
 package lk.ijse.posreactspringbootbackend.dao;
 
-public interface ItemDAO {
+import lk.ijse.posreactspringbootbackend.entity.ItemEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ItemDAO extends JpaRepository<ItemEntity, Integer> {
 }

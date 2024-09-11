60d37096866e2d96439f81de9728093b80dc686e
UchithmaSenevirathne <uchithma@gmail.com>
2024-09-11 19:37:10 +0530

    update base64

--- src/main/java/lk/ijse/posreactspringbootbackend/util/AppUtil.java
+++ src/main/java/lk/ijse/posreactspringbootbackend/util/AppUtil.java
@@ -3,7 +3,7 @@ package lk.ijse.posreactspringbootbackend.util;
 import java.util.Base64;
 
 public class AppUtil {
-    public static String toBase64ProfilePic(String profilePic){
-        return Base64.getEncoder().encodeToString(profilePic.getBytes()); //profile pic eke bytes tika aragena(profilepic eka string wunata eke byte type eka thamai ganne) eka Base64 bawata path karanawa
+    public static String toBase64ProfilePic(String image){
+        return Base64.getEncoder().encodeToString(image.getBytes()); //image eke bytes tika aragena(profilepic eka string wunata eke byte type eka thamai ganne) eka Base64 bawata path karanawa
     }
 }

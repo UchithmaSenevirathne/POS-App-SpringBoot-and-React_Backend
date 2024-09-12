dc8f7ca9a933bfd2299cdce1f35529d180c8f2f1
UchithmaSenevirathne <uchithma@gmail.com>
2024-09-12 21:29:31 +0530

    create order dto

--- src/main/java/lk/ijse/posreactspringbootbackend/dto/OrderDTO.java
+++ src/main/java/lk/ijse/posreactspringbootbackend/dto/OrderDTO.java
@@ -1,4 +1,17 @@
 package lk.ijse.posreactspringbootbackend.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class OrderDTO {
+    private int order_id;
+    private int user_id;
+    private int product_id;
+    private int quantity;
+    private double total_price;
+    private String order_date;
 }

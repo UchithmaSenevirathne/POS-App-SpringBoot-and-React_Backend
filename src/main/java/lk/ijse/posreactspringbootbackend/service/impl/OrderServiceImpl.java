6e00ab4061d6cdc0bd982ee6daedfa11ff250dec
UchithmaSenevirathne <uchithma@gmail.com>
2024-09-13 16:13:20 +0530

    create order serviceimpl

---
+++ src/main/java/lk/ijse/posreactspringbootbackend/service/impl/OrderServiceImpl.java
@@ -0,0 +1,10 @@
+package lk.ijse.posreactspringbootbackend.service.impl;
+
+import lk.ijse.posreactspringbootbackend.service.OrderService;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+public class OrderServiceImpl implements OrderService {
+}

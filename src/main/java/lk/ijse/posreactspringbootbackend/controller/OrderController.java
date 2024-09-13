7b8c7c00b0fa8dd72ec9b23c25b3a774924c0959
UchithmaSenevirathne <uchithma@gmail.com>
2024-09-13 16:08:50 +0530

    create order controller

---
+++ src/main/java/lk/ijse/posreactspringbootbackend/controller/OrderController.java
@@ -0,0 +1,13 @@
+package lk.ijse.posreactspringbootbackend.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/backend/orders")
+@CrossOrigin
+@RequiredArgsConstructor
+public class OrderController {
+}

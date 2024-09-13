fa49a43de4893076d6e9180d4b7918a496bd11c8
UchithmaSenevirathne <uchithma@gmail.com>
2024-09-13 16:27:17 +0530

    inject daos

--- src/main/java/lk/ijse/posreactspringbootbackend/service/impl/OrderServiceImpl.java
+++ src/main/java/lk/ijse/posreactspringbootbackend/service/impl/OrderServiceImpl.java
@@ -1,10 +1,26 @@
 package lk.ijse.posreactspringbootbackend.service.impl;
 
+import lk.ijse.posreactspringbootbackend.dao.ItemDAO;
+import lk.ijse.posreactspringbootbackend.dao.OrderDAO;
+import lk.ijse.posreactspringbootbackend.dao.UserDAO;
+import lk.ijse.posreactspringbootbackend.dto.OrderDTO;
 import lk.ijse.posreactspringbootbackend.service.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @Transactional
 public class OrderServiceImpl implements OrderService {
+    @Autowired
+    private OrderDAO orderDAO;
+    @Autowired
+    private ItemDAO itemDAO;
+    @Autowired
+    private UserDAO userDAO;
+
+    @Override
+    public void saveOrder(OrderDTO orderDTO) {
+
+    }
 }
